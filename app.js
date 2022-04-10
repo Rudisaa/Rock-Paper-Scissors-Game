5261a05e18b6c9782078bf5ef7526da1bb45525d
Rudis <rudisarg@gmail.com>
2022-04-10 13:26:52 -0400

    completed 1 round of game function

--- app.js
+++ app.js
@@ -4,19 +4,45 @@ let compWins = 0;
 
 function computerPlay() {
     let object = Math.floor(Math.random() * (4 - 1) + 1);
-    console.log(object);
     if(object == 1) {
         return 'rock';
     } else if(object ==  2) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
+
+
 const playerSelection = 'rock';
 function round(playerSelection, computerSelection) {
-    if(playerSelection == 'Rock' && computerSelection == 'Paper') {
-        console.log("You Lose! Paper beats rock.")
+    if(playerSelection == 'rock') {
+        if(computerSelection == 'paper') {
+            console.log('Sorry you lose! Paper Beats Rock');
+            
+        } else if(computerSelection == 'scissors') {
+            console.log("You Win! Rock beats Scissors!");
+        
+        } else {
+            console.log("It was a Tie!");
+        }
+    } else if(playerSelection == 'paper') {
+        if(computerSelection == 'rock') {
+            console.log("You Win! Paper Beats Rock");
+        } else if(computerSelection == 'paper') {
+            console.log("It was a tie!");
+        } else {
+            console.log("You Lose! Scissor beats Paper!");
+        }
+    } else if(playerSelection == 'scissors') {
+        if( computerSelection == 'rock') {
+            console.log("You Lose! Rock beats Scissors!");
+        } else if(computerSelection == 'paper') {
+            console.log("You Win! Scissors Beats Paper!");
+        } else {
+            console.log("It was a tie!");
+        }
     }
-
 }
+
+console.log(round(playerSelection, computerPlay()));
