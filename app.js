84447845d33ff56058012f8faa91f9512bf9fb5a
Rudis <rudisarg@gmail.com>
2022-05-12 16:21:30 -0400

    start over

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
-const game = () => { 
-
-    let computerOptions = ['rock', 'paper', 'scissors'];
-
-
-    let randomNumber = Math.floor(Math.random() * 3);
-    let computerHand = computerOptions[randomNumber]
-
-    compareHands(userSelection(), computerHand)
-    console.log(computerHand)
-    console.log(randomNumber);
-
-}
-
-let userSelection = () => {
-    // selecting rock, paper, scissors buttons
-    const userOption = document.querySelectorAll('#user');
-    // loop length of buttons with id of user
-    for(let i = 0; i < userOption.length; i++) {
-        // when user clicks hand button assign value to handType and return it
-        userOption[i].addEventListener('click', function() {
-            let handType = this.value;
-            console.log(handType);
-            return handType;
-            
-        });
-    }
-}
-
-function compareHands(userHand, computerHand) {
-    // combining userhand and computerhand to compare
-    let compare = userHand+computerHand
-}
-
-
-game();
+const userSelection = document.querySelectorAll('#user');
+let playerScore = 0;
+let computerScore = 0;
 
 
 
