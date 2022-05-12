56200005cd7f37c1d345a68ab6f8942351d4c505
Rudis <rudisarg@gmail.com>
2022-05-12 16:54:44 -0400

    created computer hand method and userhand

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-const userSelection = document.querySelectorAll('#user');
 let playerScore = 0;
 let computerScore = 0;
+let computerOptions = ['rock', 'paper', 'scissors'];
+let playerOptions = "";
+
+let game = () => {
+    userHand();
+}
 
 
+let buttonClick = () => {
+    console.log(computerHand());
+    console.log(playerOptions);
+}
+//user hand
+let userHand = () => {
+    const userSelection = document.querySelectorAll('#user');
+    for(let i = 0; i < userSelection.length; i++) {
+        userSelection[i].addEventListener('click', buttonClick);
+        playerOptions = userSelection[i].textContent.trim().toLowerCase();
+    }
+}
 
+//computer hand 
+let computerHand = () => {
+    let randomNumber = Math.floor(Math.random() * 3);
+    let compHand = computerOptions[randomNumber];
+    return compHand;
+}
+game();
+
+//comparing both hands
 
