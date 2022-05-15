f2b56df4fc8325661d3c0e0394b147aa61c3618e
Rudis <rudisarg@gmail.com>
2022-05-15 14:20:21 -0400

    added parameter to user function to get user hand

--- app.js
+++ app.js
@@ -3,30 +3,57 @@ let computerScore = 0;
 let computerOptions = ['rock', 'paper', 'scissors'];
 let playerOptions = "";
 
-let game = () => {
-    userHand();
+let game = () => {  
+    user();
 }
 
 
 let buttonClick = () => {
-    console.log(computerHand());
-    console.log(playerOptions);
+    
+    compareHand(playerOptions, computer());
 }
+
 //user hand
-let userHand = () => {
+let user = (userHand) => {
+   
     const userSelection = document.querySelectorAll('#user');
     for(let i = 0; i < userSelection.length; i++) {
         userSelection[i].addEventListener('click', buttonClick);
-        playerOptions = userSelection[i].textContent.trim().toLowerCase();
+        playerOptions = userHand;
     }
 }
 
 //computer hand 
-let computerHand = () => {
+let computer = () => {
     let randomNumber = Math.floor(Math.random() * 3);
-    let compHand = computerOptions[randomNumber];
-    return compHand;
+    let hand = computerOptions[randomNumber];
+    return hand;
 }
+
+//compare hands
+let compareHand = (userHand, computerHand) => {
+    let compare = userHand+computerHand;
+
+    switch(compare) {
+        case 'rockscissors':
+        case 'paperrock':
+        case 'scissorspaper':
+            console.log('winner');
+            break;
+        case 'scissorsrock':
+        case 'rockpaper':
+        case 'papersissors':
+            console.log('loser');
+            break;
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+            console.log('draw');
+            break;
+    }
+
+}
+
 game();
 
 //comparing both hands
