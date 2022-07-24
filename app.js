5667f943fdea28e6927962a2bd093e907909882b
Rudis <rudisarg@gmail.com>
2022-07-24 02:53:55 -0400

    added user, computer score, and round count variables

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-//variables - computer score, user score, 
+//variables - computer score, user score, total round count
+let userScore = 0;
+let ComputerScore = 0;
+let roundCount = 0;
+
 
 //event listener to see what type of hand was clicked
 
@@ -6,6 +10,8 @@
 
 //win function - add one to the user score
 //lose function - add one to the computer score 
+
+//game function - once 5 rounds have been reached announce the winner
 
 //style function - adding animations depending on what hand the user and the computer has - even listener - transitionend
 
