8b616356c56f60858f1a54d7baa20dfaf86f0e57
Rudis <rudisarg@gmail.com>
2022-07-24 02:52:32 -0400

    added comments beforehand

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
+//variables - computer score, user score, 
 
+//event listener to see what type of hand was clicked
+
+//function to compare hands using switch statements, when comparing call a lose function or a win function
+
+//win function - add one to the user score
+//lose function - add one to the computer score 
+
+//style function - adding animations depending on what hand the user and the computer has - even listener - transitionend
 
